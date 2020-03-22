fad5e9e4e0576f91343f995fac02f46f2ef9d235
Steven Penny <svnpenn+muv1@gmail.com>
2020-03-22 07:40:13 -0500

    deleted:    content/talks/d-curl-static/std-net-curl/test.sh

--- content/talks/d-curl-static/std-net-curl/test.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/dash
-rm -f /Root/dmd2/windows/bin64/libcurl.dll
-cp /Path/curl/libcurl.a curl.lib
-dmd test.d -m64 curl.lib
-./test # Failed to load curl, tried "libcurl.dll", "curl.dll".
-rm curl.lib test.exe test.obj
