0403cad62aa4191071e6c983c3b32549387eefaf
Steven Penny <svnpenn+muv1@gmail.com>
2020-04-25 21:56:28 -0500

    deleted:    content/rubber-duck/methods-array/index.md

--- content/rubber-duck/methods-array/b.php
+++
@@ -1,9 +0,0 @@
-<?php
-foreach ($a1 as $n1 => $a2) {
-   if ($n1 == 0) {
-      $a3 = $a2;
-   } else {
-      $n2 = array_search('fri', $a3);
-      echo $a2[$n2], "\n";
-   }
-}
--- content/rubber-duck/methods-array/c.php
+++
@@ -1,8 +0,0 @@
-<?php
-foreach ($a1 as $n1 => $a2) {
-   if ($n1 == 0) {
-      $m1 = array_flip($a2);
-   } else {
-      echo $a2[$m1['fri']], "\n";
-   }
-}
