a65ca7e10b8a037c12c2613eaaa423cf64884c03
Steven Penny <svnpenn+muv1@gmail.com>
2020-05-01 00:36:43 -0500

    +talk/**/* linguist-vendored

--- talk/convert-radix/a.py
+++ talk/convert-radix/a.py
@@ -1,3 +1,12 @@
 import string
-s1 = string.ascii_letters
+def radix64(n_in):
+   s_safe = string.digits + string.ascii_letters + '-_'
+   n_safe = len(s_safe)
+   s_out = ''
+   while n_in > 0:
+      s_out = s_safe[n_in % n_safe] + s_out
+      n_in //= n_safe
+   return s_out
+n1 = 1588306986
+s1 = radix64(n1)
 print(s1)
