87e028ed1899bfad67a9caf37b0d15be4434a2de
Steven Penny <svnpenn+muv1@gmail.com>
2020-05-25 01:27:55 -0500

    +from http import cookiejar

--- content/http-post/a.py
+++ content/http-post/a.py
@@ -1,14 +1,22 @@
+from http import cookiejar
 from urllib import request, parse
 import re
+o1 = request.build_opener(
+   request.HTTPSHandler(debuglevel=1),
+   request.HTTPCookieProcessor(cookiejar.CookieJar())
+)
 # GET
-o1 = request.urlopen('https://github.com')
-s_log = o1.read().decode()
+s_log = o1.open('https://github.com').read().decode()
 # POST
 a_auth = re.findall('data-csrf="true" value="([^"]*)"', s_log)
 m_data = {}
 m_data['value'] = 'aa540'
 m_data['authenticity_token'] = a_auth[0]
-y1 = parse.urlencode(m_data).encode()
-s_url = 'https://github.com/signup_check/username'
-o2 = request.Request(s_url, data=y1, method='POST')
-request.urlopen(o2)
+s1 = parse.urlencode(m_data)
+s2 = s1.encode()
+o2 = request.Request(
+   'https://github.com/signup_check/username',
+   data=s2,
+   headers={'Accept': '*/*'}
+)
+o1.open(o2)
