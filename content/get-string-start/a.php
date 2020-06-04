92461c9e156af51867608208cdaff7666554cebc
Steven Penny <svnpenn+nu8@gmail.com>
2020-06-04 11:05:19 -0500

    +$s = 'Sunday';

--- content/get-string-start/a.php
+++ content/get-string-start/a.php
@@ -1,10 +1,10 @@
 <?php
-$s1 = 'Sunday';
+$s = 'Sunday';
 # example 1
-$s2 = substr($s1, 0, 1);
+$s2 = substr($s, 0, 1);
 # example 2
-$s3 = $s1[0];
+$s3 = $s[0];
 # example 3
-$s4 = substr($s1, 0, 2);
+$s4 = substr($s, 0, 2);
 # print
 var_dump($s2 == 'S', $s3 == 'S', $s4 == 'Su');
