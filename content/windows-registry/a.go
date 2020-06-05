5167e75ef4ffae35ec768a65a6cd8db624040ab3
Steven Penny <svnpenn+nu8@gmail.com>
2020-06-05 00:20:29 -0500

    deleted:    content/windows-registry/a.go

--- content/windows-registry/a.go
+++
@@ -1,9 +0,0 @@
-package main
-import o1 "golang.org/x/sys/windows/registry"
-func main() {
-   o2, e := o1.OpenKey(o1.CURRENT_USER, "Console", o1.SET_VALUE)
-   if e != nil {
-      log.Fatal(e)
-   }
-   o2.SetDWordValue("CodePage", 65001)
-}
