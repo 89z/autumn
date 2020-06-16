034ff444b7a5ea7fd4e5a9b8b18334cf1dd45ec8
Steven Penny <svnpenn+nu8@gmail.com>
2020-06-16 12:53:51 -0500

    deleted:    content/talk/command-line-args/index.md

--- content/talk/command-line-args/main.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-#include <windows.h>
-int main() {
-   int argc;
-   wchar_t **argv_utf16 = CommandLineToArgvW(GetCommandLineW(), &argc);
-   char* arg_utf8;
-   WideCharToMultiByte(CP_UTF8, 0, argv_utf16[1], -1, arg_utf8, 3, 0, 0);
-   printf("%s\n", arg_utf8);
-}
