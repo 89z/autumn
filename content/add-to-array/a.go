d72848614610bafcedeb897e18d98b7ac3424885
Steven Penny <svnpenn+nu8@gmail.com>
2020-07-02 09:40:20 -0500

    +   a1 := []int{10, 11}

--- content/add-to-array/a.go
+++ content/add-to-array/a.go
@@ -2,11 +2,11 @@ package main
 import "fmt"
 func main() {
    // example 1
-   a1 := []string{"Sun"}
-   a1 = append(a1, "Mon")
+   a1 := []int{10, 11}
+   a1 = append(a1, 12)
    // example 2
-   a2 := []string{"Sun"}
-   a2 = append(a2, []string{"Mon"}...)
+   a2 := []int{10, 11}
+   a2 = append(a2, []int{12, 13}...)
    // print
    fmt.Println(a1, a2)
 }
