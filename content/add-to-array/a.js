bb23bc8cbe6db6bccbc51e05a131406677b4616d
Steven Penny <svnpenn+nu8@gmail.com>
2020-07-03 09:29:34 -0500

    +let a1 = [10, 11];

--- content/add-to-array/a.js
+++ content/add-to-array/a.js
@@ -1,11 +1,11 @@
 // example 1
-let a1 = ['Sun'];
-a1.push('Mon');
+let a1 = [10, 11];
+a1.push(12);
 // example 2
-let a2 = ['Sun'];
-a2 = a2.concat(['Mon']);
+let a2 = [10, 11];
+a2 = a2.concat([12, 13]);
 // example 3
-let a3 = ['Sun'];
-a3 = [...a3, 'Mon'];
+let a3 = [10, 11];
+a3 = [...a3, 12];
 // print
 console.log(a1, a2, a3);
