3e0f50d240249b06e888b2e19546153d0e9e858c
Steven Penny <svnpenn+nu8@gmail.com>
2020-07-07 08:55:26 -0500

    +$a1 = [10, 11];

--- content/add-to-array/a.php
+++ content/add-to-array/a.php
@@ -1,12 +1,12 @@
 <?php
 # example 1
-$a1 = ['Sun'];
-array_push($a1, 'Mon');
+$a1 = [10, 11];
+array_push($a1, 12);
 # example 2
-$a2 = ['Sun'];
-$a2[] = 'Mon';
+$a2 = [10, 11];
+$a2[] = 12;
 # example 3
-$a3 = ['Sun'];
-$a3 = array_merge($a3, ['Mon']);
+$a3 = [10, 11];
+$a3 = array_merge($a3, [12, 13]);
 # print
 var_dump($a1, $a2, $a3);
