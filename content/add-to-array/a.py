28f81890387abc5fd362ffa4df8bd59d3a63f8b7
Steven Penny <svnpenn+nu8@gmail.com>
2020-07-08 10:46:42 -0500

    +a1 = [10, 11]

--- content/add-to-array/a.py
+++ content/add-to-array/a.py
@@ -1,14 +1,14 @@
 # example 1
-a1 = ['Sun']
-a1.append('Mon')
-# exmaple 2
-a2 = ['Sun']
-a2.extend(['Mon'])
+a1 = [10, 11]
+a1.append(12)
+# example 2
+a2 = [10, 11]
+a2.extend([12, 13])
 # example 3
-a3 = ['Sun']
-a3 = [*a3, 'Mon']
+a3 = [10, 11]
+a3 = [*a3, 12]
 # example 4
-a4 = ['Sun']
-a4 += ['Mon']
+a4 = [10, 11]
+a4 += [12, 13]
 # print
 print(a1, a2, a3, a4)
