7b23423bd20801306b9b13f30199c5a77b4ec857
Steven Penny <SPenny@nch.com>
2020-07-27 09:52:25 -0500

    +   m := map[string]int{"Sunday": 10}

--- content/get-map-value/a.go
+++ content/get-map-value/a.go
@@ -1,10 +1,10 @@
 package main
 func main() {
-   m1 := map[string]int{"Sunday": 10}
+   m := map[string]int{"Sunday": 10}
    // example 1
-   n1, b1 := m1["Sunday"]
+   n1, b := m["Sunday"]
    // example 2
-   n2 := m1["Sunday"]
+   n2 := m["Sunday"]
    // print
-   println(n1, b1, n2)
+   println(n1, b, n2)
 }
