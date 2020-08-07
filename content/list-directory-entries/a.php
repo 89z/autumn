8677c13332f1ed4a9a2a6656fe8ca2d614691b6a
Steven Penny <svnpenn+nu8@gmail.com>
2020-08-07 10:18:37 -0500

    +$a = glob('a.*');

--- content/list-directory-entries/a.php
+++ content/list-directory-entries/a.php
@@ -1,13 +1,3 @@
 <?php
-# example 2
-$r1 = opendir('.');
-while (true) {
-   $s1 = readdir($r1);
-   if ($s1 === false) {
-      break;
-   }
-   var_dump($s1);
-}
-# example 3
-$a2 = glob('a.*');
-print_r($a2);
+$a = glob('a.*');
+print_r($a);
