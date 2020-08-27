417811f3dc83a42aa8b4bd22f7c4c4c8cf9062cc
Steven Penny <svnpenn+nu8@gmail.com>
2020-08-27 00:09:16 -0500

    +$f_filter = fn ($o_file) => $o_file->getFilename() == '.git' ? false : true;

--- content/list-directory-entries/a.php
+++ content/list-directory-entries/a.php
@@ -12,5 +12,13 @@ $o_iter = new RecursiveIteratorIterator($o_dir);
 foreach ($o_iter as $o_name) {
    $a3[] = $o_name->getFilename();
 }
+# example 4
+$f_filter = fn ($o_file) => $o_file->getFilename() == '.git' ? false : true;
+$o_dir = new RecursiveDirectoryIterator('.');
+$o_filter = new RecursiveCallbackFilterIterator($o_dir, $f_filter);
+$o_iter = new RecursiveIteratorIterator($o_filter);
+foreach ($o_iter as $o_name) {
+   $a4[] = $o_name->getFilename();
+}
 # print
-var_dump($a1, $a2, $a3);
+var_dump($a1, $a2, $a3, $a4);
