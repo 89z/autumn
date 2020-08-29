38c0e6b6f922fb31657a90a64d44e35c81e4c15d
Steven Penny <svnpenn+nu8@gmail.com>
2020-08-29 17:11:16 -0500

    new file:   content/get-path-middle/index.md

---
+++ content/get-path-middle/b.go
@@ -0,0 +1,12 @@
+package main
+import "path/filepath"
+func main() {
+   s := `C:\a\b.tar.xz`
+   // example 1
+   // example 2
+   s3 := filepath.Base(s)
+   // example 3
+   s4 := filepath.Ext(s)
+   // print
+   println(s1 == `C:\a\`, s2 == "b.tar.xz", s3 == "b.tar.xz", s4 == ".xz")
+}
