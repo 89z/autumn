cb2602cc49c83bf41a6a413cdd3cb633b7d0a41b
Steven Penny <svnpenn+nu8@gmail.com>
2020-08-30 00:08:11 -0500

    +   o_csv, e := os.Open("a.csv")

--- content/decode-csv/a.go
+++ content/decode-csv/a.go
@@ -6,24 +6,24 @@ import (
    "os"
 )
 func main() {
-   o_db, e := os.Open("a.csv")
+   o_csv, e := os.Open("a.csv")
    if e != nil {
       log.Fatal(e)
    }
-   o_tab := csv.NewReader(o_db)
-   a_head, e := o_tab.Read()
+   o_table := csv.NewReader(o_csv)
+   a_head, e := o_table.Read()
+   if e != nil {
+      log.Fatal(e)
+   }
+   a_body, e := o_table.ReadAll()
    if e != nil {
       log.Fatal(e)
    }
    m_row := map[string]string{}
-   for {
-      a_row, e := o_tab.Read()
-      if e != nil {
-         break
+   for n_row, a_row := range a_body {
+      for n_col, s_col := range a_head {
+         m_row[s_col] = a_row[n_col]
       }
-      for n_row, s_row := range a_head {
-         m_row[s_row] = a_row[n_row]
-      }
-      fmt.Println(m_row)
+      fmt.Println(n_row, m_row)
    }
 }
