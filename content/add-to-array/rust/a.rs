75debcd904109fdbb76942eaae869948ebf6413d
Steven Penny <svnpenn+nu8@gmail.com>
2020-10-21 08:42:35 -0500

    +   let mut a1: Vec<u8> = Vec::new();

--- content/add-to-array/rust/a.rs
+++ content/add-to-array/rust/a.rs
@@ -1,5 +1,10 @@
 fn main() {
-   let mut a = vec![10, 11];
-   a.push(12);
-   println!("{:?}", a);
+   // example 1
+   let mut a1: Vec<u8> = Vec::new();
+   a1.push(10);
+   // example 2
+   let mut a2 = vec![10, 11];
+   a2.push(12);
+   // print
+   println!("{:?}{:?}", a1, a2);
 }
