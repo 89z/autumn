65453ba614854d13fa596117481d874fd480062d
Steven Penny <svnpenn+nu8@gmail.com>
2020-10-24 00:41:40 -0500

    +   let e_dir = fs::read_dir(".")?;

--- content/list-directory-entries/_index.md
+++ content/list-directory-entries/_index.md
@@ -1,6 +1,6 @@
 ---
 title: List directory entries
 categories: [system]
-date: 2020-09-27
+date: 2020-10-24
 example: //github.com/nu8/cove/blob/master/lang-stats/lang-stats.go
 ---
--- content/list-directory-entries/rust/a.rs
+++ content/list-directory-entries/rust/a.rs
@@ -1,9 +1,10 @@
 use std::{fs, io};
 
-fn main() -> io::Result<()> {
-   for r_ent in fs::read_dir(".")? {
-      let s_ent = r_ent?.path();
-      println!("{:?}", s_ent);
+fn main() -> Result<(), io::Error> {
+   let e_dir = fs::read_dir(".")?;
+   for e_ent in e_dir {
+      let o_ent = e_ent?;
+      println!("{:?}", o_ent);
    }
    Ok(())
 }
