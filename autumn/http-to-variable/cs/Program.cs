3db8460a8fcdde5af6389ba593d5b7a618c8fb6f
Steven Penny <0ua8nj@gmail.com>
2020-11-19 08:05:59 -0600

    +      var url_s = "https://speedtest.lax.hivelocity.net";

--- autumn/http-to-variable/cs/Program.cs
+++ autumn/http-to-variable/cs/Program.cs
@@ -3,9 +3,9 @@ using System;
 
 class Program {
    static void Main() {
-      var s = "https://speedtest.lax.hivelocity.net";
+      var url_s = "https://speedtest.lax.hivelocity.net";
       var o = new WebClient();
-      var s1 = o.DownloadString(s);
-      Console.Write(s1);
+      var res_s = o.DownloadString(url_s);
+      Console.Write(res_s);
    }
 }
