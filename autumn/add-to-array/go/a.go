95a52a0ab194516524c20f7b24e99bd7bf2fb82e
Steven Penny <srpen6@gmail.com>
2020-12-15 00:04:11 -0600

    +   a = append(a, 11, 12)

--- autumn/add-to-array/go/a.go
+++ autumn/add-to-array/go/a.go
@@ -6,8 +6,10 @@ func main() {
    // example 1
    a = append(a, 10)
    // example 2
-   a2 := []int{11, 12}
-   a = append(a, a2...)
+   a = append(a, 11, 12)
+   // example 3
+   a3 := []int{13, 14}
+   a = append(a, a3...)
    // print
    fmt.Println(a)
 }
--- content/add-to-array/_index.md
+++ content/add-to-array/_index.md
@@ -1,6 +1,6 @@
 +++
 title = "Add to array"
 categories = [ "array" ]
-date = 2020-09-30
+date = 2020-12-14
 example = "cove/blob/master/byte-year/byte-year.php"
 +++
