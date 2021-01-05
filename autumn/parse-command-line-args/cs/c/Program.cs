eaf213a20cb13c0cf949b7c466ccb0247368385f
Steven Penny <srpen6@gmail.com>
2021-01-04 23:18:31 -0600

    deleted:    autumn/parallel-declare/cs/Program.cs

--- autumn/parse-command-line-args/cs/c/Program.cs
+++
@@ -1,9 +0,0 @@
-// dotnet add package System.CommandLine.DragonFruit --prerelease
-using C = System.Console;
-using System.CommandLine.DragonFruit;
-
-class Program {
-   static void Main(string argument, string prefix, string suffix) {
-      C.WriteLine(prefix + argument + suffix);
-   }
-}
--- autumn/path-to-extension/cs/Program.cs
+++
@@ -1,9 +0,0 @@
-using C = System.Console;
-using P = System.IO.Path;
-
-class Program {
-   static void Main() {
-      var s = P.GetExtension("Program.cs");
-      C.WriteLine(s == ".cs");
-   }
-}
