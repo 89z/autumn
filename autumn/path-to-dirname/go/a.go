1d9f50c6d5a4a9fc9b82f4a17573483ccdf2852c
Steven Penny <srpen6@gmail.com>
2021-01-29 07:32:49 -0600

    +   file := "C:/go/bin/go.exe"

--- autumn/path-to-dirname/go/a.go
+++ autumn/path-to-dirname/go/a.go
@@ -2,7 +2,7 @@ package main
 import "path"
 
 func main() {
-   s := "C:/go/bin/go.exe"
-   s1 := path.Dir(s)
-   println(s1 == "C:/go/bin")
+   file := "C:/go/bin/go.exe"
+   dir := path.Dir(file)
+   println(dir == "C:/go/bin")
 }
--- autumn/path-to-dirname/go/b.go
+++ autumn/path-to-dirname/go/b.go
@@ -2,7 +2,7 @@ package main
 import "path/filepath"
 
 func main() {
-   path_s := `C:\go\bin\go.exe`
-   dir_s := filepath.Dir(path_s)
-   println(dir_s == `C:\go\bin`)
+   file := `C:\go\bin\go.exe`
+   dir := filepath.Dir(file)
+   println(dir == `C:\go\bin`)
 }
