865b94aa996d1a5859de41b0906b6d667c2b521a
Steven Penny <srpen6@gmail.com>
2021-02-01 20:37:46 -0600

    deleted:    autumn/url-properties/py/a.py

--- autumn/url-properties/py/a.py
+++
@@ -1,4 +0,0 @@
-from urllib import parse
-s = 'one=odd&two=even'
-m = parse.parse_qs(s)
-print(m)
--- autumn/url-properties/py/g.py
+++
@@ -1,8 +0,0 @@
-from urllib import parse
-
-u = parse.SplitResult(
-   'http', 'docs.python.org', '/library', 'month=May&day=Friday', fragment=''
-)
-
-s = parse.urlunsplit(u)
-print(s)
