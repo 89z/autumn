40d7a1150190b4df95362fbc35f27a2849b85653
Steven Penny <srpen6@gmail.com>
2021-02-23 08:48:15 -0600

    +String format(Duration d) {

--- autumn/return-duration-string/dart/a.dart
+++ autumn/return-duration-string/dart/a.dart
@@ -1,17 +1,17 @@
 import 'dart:io';
 
-String format(Duration o) {
-   var mil_s = (o.inMilliseconds % 1000).toString().padLeft(3, '0');
-   var sec_s = (o.inSeconds % 60).toString().padLeft(2, '0');
-   return o.inMinutes.toString() + ' m ' + sec_s + ' s ' + mil_s + ' ms';
+String format(Duration d) {
+   var mil = (d.inMilliseconds % 1000).toString().padLeft(3, '0');
+   var sec = (d.inSeconds % 60).toString().padLeft(2, '0');
+   return d.inMinutes.toString() + ' m ' + sec + ' s ' + mil + ' ms';
 }
 
 void main() {
-   var o = new Stopwatch();
-   o.start();
+   var sw = new Stopwatch();
+   sw.start();
    while (true) {
       sleep(Duration(milliseconds: 10));
-      var s = format(o.elapsed);
-      stdout.write(s + '\r');
+      var elapsed = format(sw.elapsed);
+      stdout.write(elapsed + '\r');
    }
 }
