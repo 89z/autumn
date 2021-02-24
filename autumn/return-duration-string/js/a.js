0e144dbebfa7de0fbb9e240e0cbb376eefdee7a3
Steven Penny <srpen6@gmail.com>
2021-02-24 09:37:13 -0600

    +   let mil = String(n % 1000).padStart(3, '0');

--- autumn/return-duration-string/js/a.js
+++ autumn/return-duration-string/js/a.js
@@ -1,11 +1,11 @@
 'use strict';
 
 function format(n) {
-   let mil_s = String(n % 1000).padStart(3, '0');
+   let mil = String(n % 1000).padStart(3, '0');
    n = Math.trunc(n / 1000);
-   let sec_s = String(n % 60).padStart(2, '0');
+   let sec = String(n % 60).padStart(2, '0');
    n = Math.trunc(n / 60);
-   return String(n) + ' m ' + sec_s + ' s ' + mil_s + ' ms';
+   return String(n) + ' m ' + sec + ' s ' + mil + ' ms';
 }
 
 function main() {
