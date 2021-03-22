7c1d24bb57def744297da01be48d2fb042f80b4f
Steven Penny <srpen6@gmail.com>
2021-03-22 08:26:47 -0500

    +{ // example 1

--- autumn/declare-string/js/c.js
+++ autumn/declare-string/js/c.js
@@ -1,6 +1,8 @@
-// example 1
-let s1 = 'zero"one\\two';
-// example 2
-let s2 = "zero\"one\\two";
-// print
-console.log(s1, s2);
+{ // example 1
+   let s = 'zero"one\\two';
+   console.log(s);
+}
+{ // example 2
+   let s = "zero\"one\\two";
+   console.log(s);
+}
