8d8f9e1d26f7005eda6aa3a4d07bea614c433900
Steven Penny <srpen6@gmail.com>
2021-03-24 08:52:44 -0500

    +s = f.format(1e3);

--- autumn/human-readable-number/js/intl.js
+++ autumn/human-readable-number/js/intl.js
@@ -1,21 +1,22 @@
 let f = Intl.NumberFormat('en', { notation: 'compact' });
-{ // example 1
-   let s = f.format(1e3);
-   console.log(s == '1K');
-}
-{ // example 2
-   let s = f.format(1e6);
-   console.log(s == '1M');
-}
-{ // example 3
-   let s = f.format(1e9);
-   console.log(s == '1B');
-}
-{ // example 4
-   let s = f.format(1e12);
-   console.log(s == '1T');
-}
-{ // example 5
-   let s = f.format(1e15);
-   console.log(s == '1000T');
-}
+let s;
+
+// example 1
+s = f.format(1e3);
+console.log(s == '1K');
+
+// example 2
+s = f.format(1e6);
+console.log(s == '1M');
+
+// example 3
+s = f.format(1e9);
+console.log(s == '1B');
+
+// example 4
+s = f.format(1e12);
+console.log(s == '1T');
+
+// example 5
+s = f.format(1e15);
+console.log(s == '1000T');
