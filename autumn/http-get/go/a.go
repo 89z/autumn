143f84691f6c144000f3b73add543f2489a0e659
Steven Penny <srpen6@gmail.com>
2021-04-02 08:03:13 -0500

    +   res, e := http.Get("http://speedtest.lax.hivelocity.net")

--- autumn/http-get/go/a.go
+++ autumn/http-get/go/a.go
@@ -1,20 +1,15 @@
 package main
 
 import (
-   "io"
    "log"
    "net/http"
    "os"
 )
 
 func main() {
-   get, e := http.Get("http://speedtest.lax.hivelocity.net")
+   res, e := http.Get("http://speedtest.lax.hivelocity.net")
    if e != nil {
       log.Fatal(e)
    }
-   b, e := io.ReadAll(get.Body)
-   if e != nil {
-      log.Fatal(e)
-   }
-   os.Stdout.Write(b)
+   os.Stdout.ReadFrom(res.Body)
 }
