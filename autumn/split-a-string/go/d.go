01a6b938b422c6328137f8c81edaba7b538bec08
Steven Penny <srpen6@gmail.com>
2021-05-19 20:08:05 -0500

    +   a := strings.SplitN("west,north,east", ",", 2)

--- autumn/split-a-string/go/d.go
+++ autumn/split-a-string/go/d.go
@@ -1,26 +1,11 @@
 package main
-import "strings"
 
-type scanner struct { text, todo, sep string }
-
-func newScanner(todo, sep string) scanner {
-   return scanner{todo: todo, sep: sep}
-}
-
-func (s *scanner) scan() bool {
-   if s.todo == "" { return false }
-   a := strings.SplitN(s.todo, s.sep, 2)
-   s.text = a[0]
-   switch len(a) {
-      case 1: s.todo = ""
-      case 2: s.todo = a[1]
-   }
-   return true
-}
+import (
+   "fmt"
+   "strings"
+)
 
 func main() {
-   s := newScanner("north,east,south,west", ",")
-   for s.scan() {
-      println(s.text)
-   }
+   a := strings.SplitN("west,north,east", ",", 2)
+   fmt.Println(a) // [west north,east]
 }
